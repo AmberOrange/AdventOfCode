623fd4018c3ab3e4787877ce6d1db4daf1bc323f
Emil <emilgrenebrant@hotmail.com>
2019-01-01 01:42:39 +0100

    Solved Day 2 on my phone

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,10 +1,96 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <utility>
 
-int main(int args, char* argc)
-{
-	std::cout << "Hello! Will start on this task soon!" << std::endl;
+using namespace std;
 
-	std::cin.ignore();
+int main(){
+	cout << "Hello!\n";
+	ifstream file("data");
+	string currLine;
+	int lineCount = 0;
+	int twoCount = 0;
+	int threeCount = 0;
+  vector<pair<char,int>> charList;
+	vector<string> strList;
+	if(file.is_open()){
+    while(getline(file, currLine)){
+    strList.push_back(currLine);
+    charList.clear();
+    lineCount++;
+    for(int i=0; i<currLine.size(); i++){
+      char c = *(currLine.c_str()+i);
+      bool found = false;
+      for(int j=0; j < charList.size(); j++){
+        if(charList[j].first==c) {
+				  charList[j].second++;
+					found=true;
+					break;
+				}
+      }
+			if(!found)
+        charList.push_back(pair<char,int>(c,1));
+      //cout << c;
+    }
+    //cout << endl;
+		bool two = false;
+		bool three = false;
+		for(int i=0; i < charList.size(); i++){
+      char c = charList[i].first;
+      int count = charList[i].second;
+			if(count == 2 && !two) {
+        two = true;
+				cout << "(" << c << ") Two! ";
+      } else if(count == 3 && !three) {
+        three = true;
+				cout << "(" << c << ") Three! ";
+      }
+		}
+		if(two || three)
+		  cout << "\nList :" << currLine << endl;
+		if(two)
+      twoCount++;
+		if(three)
+      threeCount++;
+		}
+	}
+	cout << "Number of lines: " << lineCount
+					<< "\nTwo: " << twoCount
+					<< "\nThree: " << threeCount
+					<< "\nChecksum: " 
+					<< twoCount * threeCount
+					<< endl;
+
+	bool found = false;
+	pair<int,int> f;
+
+	for(int i=0; i < strList.size() && !found; i++) {
+    for(int j=i+1; j < strList.size(); j++) {
+      int miss = 0;
+      for(int k=0; k < strList[i].size(); k++) {
+        if(*(strList[i].c_str()+k) !=
+				  *(strList[j].c_str()+k)) {
+				  miss++;
+					if(miss>1)
+				    break;
+				}
+			}
+      if(miss==1) {
+        f.first = i;
+				f.second = j;
+				found = true;
+				break;
+			}
+		}
+	}
+  if(found) {
+    cout << "Found at " << f.first
+      << " and " << f.second
+			<< ":\n"
+			<< strList[f.first] << endl
+			<< strList[f.second] << endl;
+	}
 	return 0;
 }
